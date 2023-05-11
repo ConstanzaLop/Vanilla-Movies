332637743d82224a6c37930ceefeba99e8ec4c0a
juandurso <juandurso1@gmail.com>
2023-05-11 17:58:51 -0300

    vinculo modal, modifico admin

--- js/login.js
+++ js/login.js
@@ -7,45 +7,6 @@ const passInput = document.getElementById("password")
 const usuariosRegistrados = JSON.parse(localStorage.getItem('usuariosRegistrados-LS')) || [];
 
 
-// function handleSubmit() {
-//     if (userNameInput.value == "juanperez@hotmail.com" && passInput.value == "juan111") {
-//         alert("Puedes entrar")
-//         window.location.href = "http://www.google.com"
-//     }else{
-//         alert("el usuario ó la contraseña ingresados no coinciden")
-//     }
-
-//     if (userNameInput.value == "admin" && passInput.value == "admin") {
-//         alert("redirigir a pagina del administrador")
-//         window.location.href = "http://www.google.com"
-//         // alert("Puedes entrar")
-//     }else{
-//         alert("el usuario ó la contraseña ingresados no coinciden")
-//     }
-
-// }
-
-// function handleSubmit(e) {
-    
-//         if (userNameInput.value == "admin" && passInput.value == "admin") {
-//             e.preventDefault()
-//             // alert("Puedes entrar")
-//             window.location.href = "../html/admin.html"
-    
-//         }
-//         else if (usuariosRegistrados[i].user === userNameInput.value) {
-//             e.preventDefault()
-//             window.location.href = "../index.html"
-//         }
-
-
-
-//         else {
-//         alert("el usuario ó la contraseña ingresados no coinciden")
-//     }
-// }
-
-// openAI:
 
 function handleSubmit(e) {
     for (let i = 0; i < usuariosRegistrados.length; i++) {
